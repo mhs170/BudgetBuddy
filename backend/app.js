84dd51e9c99b38b52170925ead6fcba929e93701
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2024-07-09 18:58:42 -0400

    add-income, get-incomes, delete-income functions

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { db } = require('./db/db')
+const {readdirSync} = require('fs')
 const app = express()
 require('dotenv').config()
 
@@ -8,6 +9,9 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 
 const server = () => {
