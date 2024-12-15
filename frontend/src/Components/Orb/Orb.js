3ed9f25f44925bad3fd35685e21e30984bb610c4
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2024-12-15 08:23:09 -0500

    Button component

--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
-import { useWindowSize } from '../../Utils/useWindowSize';
+import { useWindowSize } from '../../utils/useWindowSize';
 
 function Orb() {
 
-    const{width, height} = useWindowSize()
+    const {width, height} = useWindowSize()
 
     console.log(width, height)
-    
+
     const moveOrb = keyframes`
         0%{
             transform: translate(0, 0);
         }
         50%{
-            transform: translate(${width}px, ${height}px);
+            transform: translate(${width}px, ${height/2}px);
         }
         100%{
             transform: translate(0, 0);
@@ -21,19 +21,19 @@ function Orb() {
     `
 
     const OrbStyled = styled.div`
-    width: 70vh;
-    height: 70vh;
-    position: absolute;
-    border-radius: 50%;
-    margin-left: -37vh;
-    margin-top: -37vh;
-    background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-    filter: blur(100px);
-    animation: ${moveOrb} 1s alternate linear infinite;
+        width: 70vh;
+        height: 70vh;
+        position: absolute;
+        border-radius: 50%;
+        margin-left: -37vh;
+        margin-top: -37vh;
+        background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
+        filter: blur(400px);
+        animation: ${moveOrb} 15s alternate linear infinite;
     `;
 
     return (
-        <OrbStyled></OrbStyled>  
+        <OrbStyled></OrbStyled>
     )
 }
 
