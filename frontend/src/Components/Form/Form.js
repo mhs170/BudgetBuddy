ba765ceba4dcfcd4438eacf000ff4826b9ab90cf
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2024-12-17 18:50:31 -0500

    finished income form

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import styled from 'styled-components'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useGlobalContext } from "../../Context/globalContext";
 
 function Form() {
+    const {addIncome} = useGlobalContext()
     const [inputState, setInputState] = useState ({
         title: '',
         amount: '',
-        date: '',
+        date: null,
         category: '',
         description: '',
     })
@@ -16,8 +20,13 @@ function Form() {
         setInputState({...inputState, [name]: e.target.value})
     }
 
+    const handleSubmit = e => {
+        e.preventDefault()
+        addIncome(inputState)
+    }
+
     return (
-        <FromStyled>
+        <FromStyled onSubmit={handleSubmit}>
             <div className="input-control">
                 <input 
                 type="text" 
@@ -26,6 +35,41 @@ function Form() {
                 placeholder="Salary Title"
                 onChange={handleInput('title')}
                 />
+            </div>
+            <div className="input-control">
+                <input 
+                type="text" 
+                value={amount}
+                name={'amount'}
+                placeholder="Salary Amount"
+                onChange={handleInput('amount')}
+                />
+            </div>
+            <div className="input-control">
+                <DatePicker 
+                    id='date'
+                    placeholderText='Enter A Date'
+                    selected={date}
+                    dateFormat="dd/MM/yyyy"
+                    onChange={(date) => {
+                        setInputState({...inputState, date: date})
+                    }}
+                />
+            </div>
+            <div className="selects input-control">
+                <select required value={category} name="category" id="category" onChange={handleInput('category')}>
+                    <option value=""  disabled >Select Option</option>
+                    <option value="salary">Salary</option>
+                    <option value="freelancing">Freelancing</option>
+                    <option value="investments">Investiments</option>
+                    <option value="stocks">Stocks</option>
+                    <option value="crypto">Crypto</option>
+                    <option value="pension">Pension</option>
+                    <option value="other">Other</option>  
+                </select>
+            </div>
+            <div className="submit-btn">
+                <button>Add Income</button>
             </div>
         </FromStyled>
     )
