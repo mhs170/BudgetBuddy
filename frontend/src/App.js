e82a7f976a3f5dcb9737a034376b3d5457ecc5c6
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2024-12-17 07:18:33 -0500

    created main container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,9 @@ function App() {
       {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
+        <main>
+          
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -28,6 +31,18 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg})
   position: relative;
+  main{
+    flex: 1;  
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow: auto;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+    }
 `;
 
 export default App;
