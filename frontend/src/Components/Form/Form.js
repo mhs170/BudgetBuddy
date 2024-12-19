8aa48e8327ecf5eab656fa368b5be9a57e0d7557
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2024-12-19 10:10:08 -0500

    refresh after adding income

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -7,7 +7,7 @@ import Button from '../Button/Button';
 import { plus } from '../../utils/icons'
 
 function Form() {
-    const {addIncome} = useGlobalContext()
+    const {addIncome, getIncomes} = useGlobalContext()
     const [inputState, setInputState] = useState ({
         title: '',
         amount: '',
@@ -25,6 +25,7 @@ function Form() {
     const handleSubmit = e => {
         e.preventDefault()
         addIncome(inputState)
+        getIncomes()
     }
 
     return (
